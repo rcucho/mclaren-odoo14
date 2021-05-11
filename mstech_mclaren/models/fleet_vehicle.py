3e2b1fc119da3d2f32a4547f927efe4a3c38b2aa
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-11 11:28:58 -0500

    Update fleet_vehicle.py

--- mstech_mclaren/models/fleet_vehicle.py
+++ mstech_mclaren/models/fleet_vehicle.py
@@ -4,6 +4,11 @@ from odoo.exceptions import UserError
 class FleetMclaren(models.Model):
     _inherit = 'fleet.vehicle'
 
+    #voucher_file_ids = fields.Many2many(comodel_name='ir.attachment', relation='payment_register_voucher_file_tabla', column1='payment_register_id', column2='attachment_id', string='Foto de Placa')
     license_plate_file_id = fields.Many2many('ir.attachment', 'license_plate_file_attachments_rel',
-                                  'license_plate', 'attachment_id', string="License plate",
-                                  help="Picture of the license plate of vehicle ")
+                              'license_plate', 'attachment_id', string="License plate",
+                              help="Picture of the license plate of vehicle")
+    acriss = fields.Char(string='ACRISS')
+    property_card = fields.Many2many('ir.attachment', 'property_card_file_attachments_rel',
+                              'property_card', 'attachment_id', string="Property card",
+                              help="Property card document")
