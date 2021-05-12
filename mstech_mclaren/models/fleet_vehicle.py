8e0d6c05ac2833050e3b279731449fd9c586a23c
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-12 13:37:41 -0500

    Update fleet_vehicle.py

--- mstech_mclaren/models/fleet_vehicle.py
+++ mstech_mclaren/models/fleet_vehicle.py
@@ -4,11 +4,25 @@ from odoo.exceptions import UserError
 class FleetMclaren(models.Model):
     _inherit = 'fleet.vehicle'
 
-    #voucher_file_ids = fields.Many2many(comodel_name='ir.attachment', relation='payment_register_voucher_file_tabla', column1='payment_register_id', column2='attachment_id', string='Foto de Placa')
     license_plate_file_id = fields.Many2many('ir.attachment', 'license_plate_file_attachments_rel',
                               'license_plate', 'attachment_id', string="License plate",
-                              help="Picture of the license plate of vehicle")
+                              help="Foto de la placa del vehiculo")
     acriss = fields.Char(string='ACRISS')
     property_card = fields.Many2many('ir.attachment', 'property_card_file_attachments_rel',
                               'property_card', 'attachment_id', string="Property card",
-                              help="Property card document")
+                              help="Documento de la tarjeta de propiedad")
+                              
+    insurance_company = fields.Char(string = "Compañia de Seguros")
+    
+    policy_number = fields.Integer(string = "Numero de Poliza")
+    policy_expiration = fields.Date(string = "Vencimiento de Poliza")
+    policy_amount = fields.Float(string = "Suma Asegurada")
+    policy_file = fields.Many2many('ir.attachment', 'policy_file_attachments_rel',
+                              'policy_number', 'attachment_id', string="Documento de poliza",
+                              help="Documento de la poliza")
+
+    soat_number = fields.Float(string = "SOAT")
+    soat_expiration = fields.Date(string = "Vencimiento de Soat")
+    soat_file = fields.Many2many('ir.attachment', 'soat_file_attachments_rel',
+                              'soat_number', 'attachment_id', string="Documento de SOAT",
+                              help="Documento del Soat")
