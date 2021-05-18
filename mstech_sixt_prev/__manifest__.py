f9c1267c8ed41f07397148d8dbd58973d9c226ea
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-18 13:38:39 -0500

    Add files via upload

---
+++ mstech_sixt_prev/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo - Sixt McLaren',
+    'category': 'Technical Configuration',
+    'author': 'Mouse Technologies',
+    'summary':'Odoo - Sixt McLaren',
+    'license': 'LGPL-3',
+    'website': 'https://www.mstech.pe',
+    'depends': [
+        'account_fleet',
+        'sale_crm',
+        'mouse_invoice_merge',
+        'mouse_einvoice_sale',
+    ],
+    'data': [
+        'data/sale_data.xml',
+        'views/sale_views.xml',
+    ],
+    'installable': True,
+    'sequence': 1,
+}
+
