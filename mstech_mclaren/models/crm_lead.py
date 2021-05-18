58bb5a8044e3606e40fc827820f26d18dddcc46e
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-18 18:18:52 -0500

    Add files via upload

---
+++ mstech_mclaren/models/crm_lead.py
@@ -0,0 +1,13 @@
+from odoo import models,fields,api,_
+from odoo.exceptions import UserError
+
+class SaleMclaren(models.Model):
+    _inherit = 'crm.lead'
+    
+    pickup_place = fields.Char(string = "Lugar de Recojo")
+    pickup_date = fields.Datetime(string = "Fecha y Hora de Recojo")
+    devoluiton_place = fields.Char(string = "Lugar de Devolucion")
+    devolution_date = fields.Datetime(string = "Fecha y Hora de Devolucion")
+    type_car = fields.Char(String = "Tipo de Carro")
+    
+    
