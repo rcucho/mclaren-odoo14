a1a25d52c766b4cf4828c15098303e50c6be40d8
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-19 13:00:07 -0500

    Update sale_order.py

--- mstech_mclaren/models/sale_order.py
+++ mstech_mclaren/models/sale_order.py
@@ -12,13 +12,20 @@ class SaleMclaren(models.Model):
     
     @api.depends('opportunity_id')
     def _compute_oportunity_data(self):
-        #if self.opportunity_id:
+       
         for record in self:
-            record.pickup_place = record.opportunity_id.pickup_place
-            record.pickup_date = record.opportunity_id.pickup_date
-            record.devolution_place = record.opportunity_id.devolution_place
-            record.devolution_date = record.opportunity_id.devolution_date 
-            record.type_car = record.opportunity_id.type_car
+            if self.opportunity_id:
+                record.pickup_place = record.opportunity_id.pickup_place
+                record.pickup_date = record.opportunity_id.pickup_date
+                record.devolution_place = record.opportunity_id.devolution_place
+                record.devolution_date = record.opportunity_id.devolution_date 
+                record.type_car = record.opportunity_id.type_car
+            else:             
+                record.pickup_place = False
+                record.pickup_date = False
+                record.devolution_place = False
+                record.devolution_date = False
+                record.type_car = False
 
 
 
