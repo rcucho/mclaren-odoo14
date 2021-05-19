4f308eec606539f7fc5da014924043491bc2ee1d
rcucho <38509900+rcucho@users.noreply.github.com>
2021-05-18 22:36:34 -0500

    Update crm_lead.py

--- mstech_mclaren/models/crm_lead.py
+++ mstech_mclaren/models/crm_lead.py
@@ -4,10 +4,10 @@ from odoo.exceptions import UserError
 class SaleMclaren(models.Model):
     _inherit = 'crm.lead'
     
-    pickup_place = fields.Char(string = "Lugar de Recojo")
-    pickup_date = fields.Datetime(string = "Fecha y Hora de Recojo")
-    devoluiton_place = fields.Char(string = "Lugar de Devolucion")
-    devolution_date = fields.Datetime(string = "Fecha y Hora de Devolucion")
-    type_car = fields.Char(string = "Tipo de Carro")
+    pickup_place = fields.Char(string="Lugar de Recojo")
+    pickup_date = fields.Datetime(string="Fecha y Hora de Recojo")
+    devoluiton_place = fields.Char(string="Lugar de Devolucion")
+    devolution_date = fields.Datetime(string="Fecha y Hora de Devolucion")
+    type_car = fields.Char(string="Tipo de Carro")
     
     
